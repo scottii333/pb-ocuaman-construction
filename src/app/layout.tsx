ace69e918ee9d69c555fcbed8ad474d9e1a27c0a
Scottiieee <splendidhoodies@gmail.com>
2025-12-14 15:46:42 +0800

    Add Header and Footer components to RootLayout for improved structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -50,9 +52,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-[200vh] flex flex-col`}
       >
-        {children}
+        <Header />
+        <main className="grow">{children}</main>
+
+        <Footer />
       </body>
     </html>
   );
