91cc350d86bf95551994f74c227da054f4bb6d47
Scottiieee <splendidhoodies@gmail.com>
2025-12-16 19:20:40 +0800

    Refactor ServiceBenefits component layout and update service descriptions for clarity

--- src/components/ServiceBenefits.tsx
+++ src/components/ServiceBenefits.tsx
@@ -6,11 +6,13 @@ import {
   faStar,
   faMoneyBillTrendUp,
 } from "@fortawesome/free-solid-svg-icons";
+import ServicesCarousel from "./ServicesCarousel";
+import { FAQ } from "./FAQ";
 
 export const ServiceBenefits = () => {
   return (
-    <section className="flex flex-col p-5 items-center justify-center">
-      <h1 className="mb-20 text-2xl font-medium text-center md:text-3xl">
+    <section className="flex flex-col p-5 justify-center gap-10">
+      <h1 className="mb-10 text-2xl font-medium text-center md:text-3xl">
         <span className="text-yellow-500">BENEFITS</span> OF OUR <br /> SERVICES
       </h1>
 
@@ -50,32 +52,36 @@ export const ServiceBenefits = () => {
             {/* Floating icon */}
             <div className="absolute -top-6 left-1/2 -translate-x-1/2 bg-[#d1d3c4] rounded-full p-3">
               <FontAwesomeIcon
-                icon={faScrewdriverWrench}
+                icon={faHouseChimney}
                 className="h-8 md:h-10 text-white"
               />
             </div>
 
             {/* Text */}
             <p className="mt-4 text-[#232b5f] font-medium text-sm md:text-xl">
-              We guide you through every step
+              Solutions made to fit your needs
             </p>
           </div>
           <div className="relative bg-[#f2f2f2] bg-[url('/texture/rocky-wall.png')] bg-repeat w-full max-w-[400px] px-6 py-10 text-center justify-center items-center flex">
             {/* Floating icon */}
             <div className="absolute -top-6 left-1/2 -translate-x-1/2 bg-[#d1d3c4] rounded-full p-3">
               <FontAwesomeIcon
-                icon={faStar}
+                icon={faMoneyBillTrendUp}
                 className="h-8 md:h-10 text-white"
               />
             </div>
 
             {/* Text */}
             <p className="mt-4 text-[#232b5f] font-medium text-sm md:text-xl">
-              High-quality work done efficiently
+              Make the most of your space and investment
             </p>
           </div>
         </div>
       </div>
+
+      <ServicesCarousel />
+
+      <FAQ />
     </section>
   );
 };
