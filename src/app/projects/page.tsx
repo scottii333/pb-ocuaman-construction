1bc20f6c339af09a8f16a8ab7fc51bf1a548e015
Scottiieee <splendidhoodies@gmail.com>
2025-12-19 04:29:18 +0800

    feat: replace project image carousel with RecentProjectCarousel component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,11 +6,7 @@ import { ExploreProject } from "@/components/ExploreProject";
 import { FAQ } from "@/components/FAQ";
 import ScrollToProjectDetails from "@/components/ScrollToProjectDetails";
 import { projectsData } from "@/data/projectsData";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowRightLong,
-  faArrowLeftLong,
-} from "@fortawesome/free-solid-svg-icons";
+import { RecentProjectCarousel } from "@/components/RecentProjectCarousel";
 
 // Get project from URL search params
 async function getProjectFromParams(
@@ -69,96 +65,7 @@ export default async function ProjectsPage({
         <div className="absolute inset-0 bg-black/60 z-10"></div>
       </div>
 
-      {/* Project Image Carousel */}
-      <div className="flex  flex-col  p-10">
-        <h3 className="text-center">
-          OUR MOST <span className="text-[#D29E34] font-semibold">RECENT</span>{" "}
-          PROJECTS
-        </h3>
-        <p className="text-center"> Crafted with quality and care.</p>
-        <div className="flex gap-5 h-[500px] w-full overflow-x-auto overflow-y-hidden p-5 snap-x snap-mandatory scroll-smooth ">
-          <div className="grid grid-cols-1 gap-2 min-w-[650px]">
-            {/* First row: 2 columns, each 100px tall */}
-            <div className="grid grid-cols-2 gap-2 w-full h-full">
-              <div className="relative w-full h-full">
-                <Image
-                  src="/sample/sample.jpg"
-                  alt="Project"
-                  fill
-                  className="object-cover rounded"
-                />
-              </div>
-              <div className="relative w-full h-full">
-                <Image
-                  src="/sample/sample.jpg"
-                  alt="Project"
-                  fill
-                  className="object-cover rounded"
-                />
-              </div>
-            </div>
-            {/* Second row: 1 column, full width, also 100px tall */}
-            <div className="relative w-full h-full">
-              <Image
-                src="/sample/sample.jpg"
-                alt="Project"
-                fill
-                className="object-cover rounded"
-              />
-            </div>
-          </div>
-          <div className="grid grid-cols-2 gap-2 min-w-[650px] ">
-            {/* Left side: one image, spans 2 rows */}
-            <div className="relative row-span-2 h-full">
-              <Image
-                src="/sample/sample.jpg"
-                alt="Project"
-                fill
-                className="object-cover rounded"
-              />
-            </div>
-
-            {/* Right side: two stacked images */}
-            <div className="relative  h-full min-h-0">
-              <Image
-                src="/sample/sample.jpg"
-                alt="Project"
-                fill
-                className="object-cover rounded"
-              />
-            </div>
-            <div className="relative  h-full min-h-0">
-              <Image
-                src="/sample/sample.jpg"
-                alt="Project"
-                fill
-                className="object-cover rounded"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="flex gap-5 justify-end mt-5">
-          <button
-            type="button"
-            aria-label="Previous"
-            title="Previous"
-            className="bg-[#8B8B6F] bg-[url('/texture/green-cup.png')] bg-repeat text-white cursor-pointer hover:bg-[#6e6e52] transition flex items-center justify-center px-10 py-2"
-            style={{ backgroundSize: "200px 200px" }}
-          >
-            <FontAwesomeIcon icon={faArrowLeftLong} className="text-3xl" />
-          </button>
-
-          <button
-            type="button"
-            aria-label="Next"
-            title="Next"
-            className="bg-[#8B8B6F] bg-[url('/texture/green-cup.png')] bg-repeat text-white cursor-pointer hover:bg-[#6e6e52] transition flex items-center justify-center px-10 py-2"
-            style={{ backgroundSize: "200px 200px" }}
-          >
-            <FontAwesomeIcon icon={faArrowRightLong} className="text-3xl" />
-          </button>
-        </div>
-      </div>
+      <RecentProjectCarousel />
 
       {/* Project Details - remove key prop */}
       <ProjectDetails data={activeProject} />
