ed12501056d4421fa07f701dbecf507f8857aa88
Scottiieee <splendidhoodies@gmail.com>
2025-12-21 04:09:26 +0800

    style: add data files for certifications, partners, and team members

---
+++ src/data/teamMembersData.tsx
@@ -0,0 +1,13 @@
+export type TeamMemberType = {
+  id: number;
+  name: string;
+  image: string;
+};
+
+export const teamMembersData: TeamMemberType[] = [
+  { id: 1, name: "Engr. Ocuaman", image: "/team-member.png" },
+  { id: 2, name: "Engr. Santos", image: "/team-member.png" },
+  { id: 3, name: "Engr. Reyes", image: "/team-member.png" },
+  { id: 4, name: "Engr. Cruz", image: "/team-member.png" },
+  { id: 5, name: "Engr. De Leon", image: "/team-member.png" },
+];
