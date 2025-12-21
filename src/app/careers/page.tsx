17e1ae0d3e775e8af73ac7d1bcf82d6b4cb11d06
Scottiieee <splendidhoodies@gmail.com>
2025-12-22 04:54:01 +0800

    style: update careers page layout for improved user engagement

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -4,6 +4,15 @@ import Image from "next/image";
 import { JoinOurTeam } from "@/components/JoinOurTeam";
 import { ApplyForJob } from "@/components/ApplyForJob";
 import { FAQ } from "@/components/FAQ";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import Link from "next/link";
 
 export default function careers() {
   return (
@@ -18,12 +27,33 @@ export default function careers() {
           className="object-cover"
         />
         <div className="absolute inset-0 bg-black/60 z-10"></div>
+
+        {/* Breadcrumbs */}
+        <Breadcrumb className="p-8 absolute top-0 left-0 text-white z-20">
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link href="/" className="text-white">
+                  Home
+                </Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator className="text-white" />
+            <BreadcrumbItem>
+              <BreadcrumbPage className="text-[#D29E34]">
+                Careers
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+
         <div className="p-8 absolute inset-0 flex flex-col items-center justify-end text-white z-10">
           <div className="text-2xl md:text-3xl">
             JOIN OUR <span className="text-[#D29E34]">TEAM</span>
           </div>
           <div className="text-sm md:text-base max-w-115 text-center">
-            Be part of a growing construction team where your skills are valued, and your work makes an impact.
+            Be part of a growing construction team where your skills are valued,
+            and your work makes an impact.
           </div>
         </div>
       </div>
