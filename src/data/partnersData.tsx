99f9e6d3d1daab111234c6c86f99cde23b7003c2
Scottiieee <angelscottdeleon@gmail.com>
2026-03-23 14:18:54 +0800

    feat: update partner names and images for improved branding and representation

--- src/data/partnersData.tsx
+++ src/data/partnersData.tsx
@@ -7,32 +7,80 @@ export type PartnerType = {
 export const partnersData: PartnerType[] = [
   {
     id: 1,
-    name: "Partner 1",
+    name: "Arkishop",
     image:
-      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1766346443/partner-1_tufqzw.png",
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774241013/ARKISHOP_LOGO_mytokj.jpg",
   },
   {
     id: 2,
-    name: "Partner 2",
+    name: "BambooHub",
     image:
-      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1766346442/partner-2_s9yksr.png",
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240972/BAMBOOHUB_LOGO_ztqir5.png",
   },
   {
     id: 3,
-    name: "Partner 3",
+    name: "Caye Woods",
     image:
-      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1766346441/partner-3_wzd9pm.png",
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240969/CAYEWOODS_LOGO_fbmxgj.jpg",
   },
   {
     id: 4,
-    name: "Partner 4",
+    name: "Davies Paint",
     image:
-      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1766346441/partner-4_a4fsw4.png",
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240968/DAVIES_PAINT_LOGO_cxm3w7.png",
   },
   {
     id: 5,
-    name: "Partner 5",
+    name: "Formica",
     image:
-      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1766346440/partner-5_njs8aa.png",
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240968/FORMICA_LOGO_lgh6aw.png",
+  },
+  {
+    id: 6,
+    name: "Greyscale",
+    image:
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240967/GREYSCALE_LOGO_sypdpq.jpg",
+  },
+  {
+    id: 7,
+    name: "Matimco",
+    image:
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240967/MATIMCO_LOGO_ltrnak.jpg",
+  },
+  {
+    id: 8,
+    name: "Polylite",
+    image:
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240963/POLYLITE_LOGO_rp5jzd.jpg",
+  },
+  {
+    id: 9,
+    name: "Polywood",
+    image:
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240962/POLYWOOD_LOGO_n2kdo9.jpg",
+  },
+  {
+    id: 10,
+    name: "RLV Roofing",
+    image:
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240960/RLV_ROOFING_LOGO_xt6p8v.jpg",
+  },
+  {
+    id: 11,
+    name: "SCM Roofing",
+    image:
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240960/SCM_ROOFING_LOGO_nfuz4m.png",
+  },
+  {
+    id: 12,
+    name: "Siemens",
+    image:
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240960/Siemens_logo_PNG1_ogunb2.png",
+  },
+  {
+    id: 13,
+    name: "Titan Concrete",
+    image:
+      "https://res.cloudinary.com/djubpvnfa/image/upload/q_auto,f_auto,dpr_auto/v1774240959/TITAN_CONCRETE_qo3rhg.jpg",
   },
 ];
